120ee62bedb1e92766ceddd2bd565b72bafea8ae
chenhaonan111 <3239676086@qq.com>
2022-07-06 01:50:59 -0400

    Site updated: 2022-07-06 01:50:57

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+//const ap = new APlayer({
+//    container: document.getElementById('aplayer'),
+//    fixed: true,
+//    autoplay: false,
+//    audio: [
+//	{
+//        name: "Welcome to New York",
+//        artist: 'A.M.7.',
+//        url: 'https://m10.music.126.net/20220706044230/7991e42746f0e1591aca8f6a99dba3a2/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/3058661233/a63d/fb0a/14bb/205f7ed13bc6e9cb5bbd94d2641b811d.mp3',
+//        cover: 'http://p1.music.126.net/aw90yskYzfcy3lNkpfmbng==/18446506579453918.jpg?param=130y130',
+//      },
+//	  {
+//        name: 'So Far Away',
+//        artist: 'Martin Garrix',
+//        url: 'https://ch-sycdn.kuwo.cn/b5690cf037a65e61ca99da9c075a3bd4/62c499b2/resource/n3/14/72/3044931198.mp3',
+//        cover: 'https://img1.kuwo.cn/star/albumcover/500/67/0/2275517451.jpg',
+//      },
+//      {
+//        name: 'Empty Love(副歌)',
+//        artist: 'Lulleaux',
+//        url: 'https://m10.music.126.net/20220706043850/80cab07ae76a9603b070e2a84368760c/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/13297626377/7b06/dfa9/71df/34f7074d25597a799a8e080bab28f7cb.mp3',
+//        cover: 'https://www.google.com.hk/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F9c%2F75%2Fce%2F9c75ce03fd02227317edb34599550356.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F724164815062549947%2F&tbnid=zC0DGDwAtYJHtM&vet=12ahUKEwjixYCLyOL4AhUJEZQKHSMEDX0QMygCegUIARCnAQ..i&docid=SXi2pvX6yLfDkM&w=731&h=498&q=Empty%20love&ved=2ahUKEwjixYCLyOL4AhUJEZQKHSMEDX0QMygCegUIARCnAQ',
+//      },
+//	  {
+//        name: 'Something Just Like This',
+//        artist: 'The Chainsmokers',
+//        url: 'http://www.ytmp3.cn/down/50463.mp3',
+//        cover: 'http://p2.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg?param=130y130',
+//      }
+//    ]
+//});
